b00380df6d77f86fe421ee85d67a0862a02cefcf
khkwan0 <khkwan0@gmail.com>
2017-10-02 10:47:43 +0700

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject road;
+    private GameObject newestRoad;
+    private GameObject oldRoad;
+    private GameObject playerCar;
+    private Camera playerCamera;
+    private GameObject grandFather;
+
+    public GameObject playerCarPrefab;
+    public GameObject roadPrefab;
+
+    public Camera playerCameraPrefab;
+
+    public float cameraHeight = 50.0f;
+
+    [SerializeField]
+    private Vector3 carLocation;
+
+	void Start () {
+        grandFather = null;
+        newestRoad = Instantiate(roadPrefab);
+        playerCar = Instantiate(playerCarPrefab, new Vector3(0.0f, 1.3f, 10.0f), Quaternion.LookRotation(Vector3.forward, Vector3.up));
+        playerCamera = Instantiate(playerCameraPrefab);
+        playerCamera.transform.Rotate(new Vector3(90.0f, 0.0f, 0.0f));
+        playerCamera.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerCamera.transform.position = new Vector3(0.0f, cameraHeight, playerCar.transform.position.z + 20.0f);
+        carLocation = playerCar.transform.position;
+	}
+
+    public void createMoreRoad()
+    {
+        oldRoad = newestRoad;   
+        newestRoad = Instantiate(roadPrefab, new Vector3(0.0f, 0.0f, newestRoad.transform.position.z + CreateRoad.height), Quaternion.LookRotation(Vector3.forward, Vector3.up));
+        if (grandFather)
+        {
+            Destroy(grandFather);
+        }
+        grandFather = oldRoad;
+    }
+}
---
+++ Assets/Scripts/VehicleControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControl : MonoBehaviour {
+
+    /*
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    */
+    private Rigidbody rb;
+    private Vector2 touchDeltaPosition;
+    [SerializeField]
+    private Vector3 newVelocity;
+
+    public float lateralMoveSpeed = 20.0f;
+    public float axialMoveSpeed = 20.0f;
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+        axialMoveSpeed = 20.0f;
+    }
+
+    public void Update()
+    {
+        newVelocity = Vector3.zero;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+             newVelocity = new Vector3(touchDeltaPosition.x, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            newVelocity = new Vector3(-lateralMoveSpeed, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            newVelocity = new Vector3(lateralMoveSpeed, 0.0f, 0.0f);
+        }
+        newVelocity += transform.forward * axialMoveSpeed;
+        rb.velocity = newVelocity;
+            
+    }
+    public void FixedUpdate()
+    {
+        /*
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+        */
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
